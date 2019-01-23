180dead0e1a02a0a810b4ef65ea131f30d6d1c1b
Anita Virag <anitavirag.dev@gmail.com>
2019-01-23 17:48:55 +0100

    adding material layout

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Layout from "./Layout";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Layout />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+if (document.getElementById("app")) {
+  ReactDOM.render(<App />, document.getElementById("app"));
 }
---
+++ resources/js/containers/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {};
+
+class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default withStyles(styles)(Home);
